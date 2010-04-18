f7ba770d8442c3126a71e33066350b09f9bae809
Abhishek Mishra <ideamonk@gmail.com>
2010-04-19 03:04:57 +0530

    planned out db schema, set up relationships, time to pause it for a while

--- dump__bugbase.sql
+++ dump__bugbase.sql
@@ -3,21 +3,134 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 18, 2010 at 03:57 PM
+-- Generation Time: Apr 19, 2010 at 03:03 AM
 -- Server version: 5.1.41
 -- PHP Version: 5.3.2-1ubuntu4
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
 --
 -- Database: `bugbase`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bughistory`
+--
+
+CREATE TABLE IF NOT EXISTS `bughistory` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `bug_id` int(11) NOT NULL,
+  `addedBy` int(11) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` int(11) NOT NULL,
+  `comment` varchar(2000) NOT NULL,
+  `assignedTo` int(11) NOT NULL,
+  `priority` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `bughistory`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bugs`
+--
+
+CREATE TABLE IF NOT EXISTS `bugs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `subject` varchar(500) NOT NULL,
+  `description` varchar(2000) NOT NULL,
+  `affectedVersion` varchar(50) NOT NULL DEFAULT 'na',
+  `belongsTo` int(11) NOT NULL DEFAULT '-1',
+  `alsoAffects` int(11) NOT NULL DEFAULT '-1',
+  `category` int(11) NOT NULL,
+  `keywords` varchar(500) NOT NULL,
+  `createdBy` int(11) NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `bugs`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `label` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `categories`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `priorities`
+--
+
+CREATE TABLE IF NOT EXISTS `priorities` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `label` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `priorities`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `projects`
+--
+
+CREATE TABLE IF NOT EXISTS `projects` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(500) NOT NULL,
+  `description` varchar(2000) NOT NULL,
+  `owner` int(11) NOT NULL,
+  `keywords` varchar(500) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `projects`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `statuses`
+--
+
+CREATE TABLE IF NOT EXISTS `statuses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `label` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `statuses`
+--
+
 
 -- --------------------------------------------------------
 
@@ -31,6 +144,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(500) NOT NULL,
   `name` varchar(500) NOT NULL,
   `email` varchar(500) NOT NULL DEFAULT 'none',
+  `is_admin` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
@@ -38,5 +152,6 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `name`, `email`) VALUES
-(1, 'user1', '5f4dcc3b5aa765d61d8327deb882cf99', 'Abhishek Mishra', 'none');
+INSERT INTO `users` (`id`, `username`, `password`, `name`, `email`, `is_admin`) VALUES
+(1, 'user1', '5f4dcc3b5aa765d61d8327deb882cf99', 'Abhishek Mishra', 'none', 0);
+
