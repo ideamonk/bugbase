9c275765116e74034b12af8cdb6953f958d5e557
Abhishek Mishra <ideamonk@gmail.com>
2010-04-21 23:18:17 +0530

    added models.php for reusable db code

--- templates_c/%%7B^7B2^7B273D6F%%addedit.html.php
+++ templates_c/%%7B^7B2^7B273D6F%%addedit.html.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version 2.6.26, created on 2010-04-21 21:15:48
+<?php /* Smarty version 2.6.26, created on 2010-04-21 23:16:11
          compiled from addedit.html */ ?>
 <div id='contentSpacer'>
 	<h2>Add a new bug</h2>
@@ -20,39 +20,39 @@
 
 			<label for='project'>Belongs to: </label>
 			<select name='project' id='project'>
-				<option>Foo</option>
-				<option>Bar</option>
-				<option>Beep</option>
+				<option value='1'>Foo</option>
+				<option value='2'>Bar</option>
+				<option value='3'>Beep</option>
 			</select>
 			
 			<label for='affectedProject'>Also affects: </label>
 			<select name='affectedProject' id='affectedProject'>
-				<option>Foo</option>
-				<option>Bar</option>
-				<option>Beep</option>
+				<option value='1'>Foo</option>
+				<option value='2'>Bar</option>
+				<option value='3'>Beep</option>
 			</select>
 			
 			<br />
 			
 			<label for='priority'>Priority: </label>
 			<select name='priority' id='priority'>
-				<option>low</option>
-				<option>med</option>
-				<option>high</option>
+				<option value='1'>low</option>
+				<option value='2'>med</option>
+				<option value='3'>high</option>
 			</select>
 			
 			<label for='bugType'>Bug type: </label>
 			<select name='bugType' id='bugType'>
-				<option>defect</option>
-				<option>feature</option>
-				<option>patch</option>
+				<option value='1'>defect</option>
+				<option value='2'>feature</option>
+				<option value='3'>patch</option>
 			</select>
 			
 			<label for='status'>Status: </label>
 			<select name='status' id='status'>
-				<option>new</option>
-				<option>duplicate</option>
-				<option>closed</option>
+				<option value='1'>new</option>
+				<option value='2'>duplicate</option>
+				<option value='3'>closed</option>
 			</select>
 			
 			<label for='keywords'>Keywords: </label>
