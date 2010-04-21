a7c0a8c62a436ce4fcfe4dcc79810ff914e942b9
Abhishek Mishra <ideamonk@gmail.com>
2010-04-22 02:24:14 +0530

    list generation still in progress

---
+++ includes/models.inc.php
@@ -0,0 +1,86 @@
+<?php
+// Reusable db code to be used in the main controllers
+
+function pickFromPost($values){
+	// picks values from POST and returns an associative array
+	$a = array();
+	foreach ($values as $key){
+		$a[$key] = $_POST[$key];
+	}
+	return $a;
+}
+
+function addNewBug(){
+	$bug = pickFromPost(array('bugName','description','affectedVersion',
+								'project','affectedProject','priority',
+										'bugType','status','keywords'));
+	$query = "INSERT INTO `bugbase`.`bugs` (`id`, `bugName`, `description`, `affectedVersion`, `project`, `affectedProject`, `bugType`, `keywords`, `createdBy`, `createdAt`) VALUES (NULL, '{$bug['bugName']}', '{$bug['description']}', '{$bug['affectedVersion']}', '{$bug['project']}', '{$bug['affectedProject']}', '{$bug['bugType']}', '{$bug['keywords']}', '{getCurrentUid()}', CURRENT_TIMESTAMP);";
+	$result = mysql_query($query) or die ("Failed to insert '{$bug['bugName']}'");
+	
+	$new_bug_id = mysql_insert_id();
+	
+	$query = "INSERT INTO `bugbase`.`bughistory` (`id`, `bug_id`, `addedBy`, `timestamp`, `status`, `comment`, `assignedTo`, `priority`) VALUES (NULL, '$new_bug_id', '{getCurrentUid()}', CURRENT_TIMESTAMP, '{$bug['status']}', '', '', '{$bug['priority']}');";
+	$result = mysql_query($query) or die ("Failed to insert '{$bug['bugName']}'");
+	
+	return $new_bug_id;
+}
+
+function getAllBugList(){
+	$buglist = array();
+	$index = 0;
+	
+	$query = "SELECT * FROM `bugs`;";
+	$result = mysql_query($query) or die ("Failed to fetch bug list");
+	while ($row = mysql_fetch_assoc($result)){
+		// augument a row with latest bughistory info
+		$query2 = "SELECT * from `bughistory` where bug_id = '{$row['id']}' order by timestamp desc;";
+		$result2 = mysql_query($query2);
+		if ($row2 = mysql_fetch_assoc($result2)){
+			foreach ($row2 as $key=>$val){
+				// merge both assoc arrays
+				if ($key!='id'){
+					$row[$key] = $val;
+				}
+			}
+		}
+		
+		// fill in status of this row
+		$query3 = "SELECT `label` from `statuses` where `id`='{$row['status']}';";
+		$result3 = mysql_query($query3);
+		if ($row3 = mysql_fetch_assoc($result3)){
+			$row['status'] = $row3['label'];
+		} else {
+			$row['status'] = 'undefined';
+		}
+		
+		// fill in priority of this row
+		$query3 = "SELECT `label` from `priorities` where `id`='{$row['priority']}';";
+		$result3 = mysql_query($query3);
+		if ($row3 = mysql_fetch_assoc($result3)){
+			$row['priority'] = $row3['label'];
+		} else {
+			$row['priority'] = ' ';
+		}
+		
+		// fill in category of this row
+		$query3 = "SELECT `label` from `categories` where `id`='{$row['bugType']}';";
+		$result3 = mysql_query($query3);
+		if ($row3 = mysql_fetch_assoc($result3)){
+			$row['bugType'] = $row3['label'];
+		} else {
+			$row['bugType'] = 'unknown';
+		}
+		
+		// fill in project name of this row
+		$query3 = "SELECT name from `projects` where `id`='{$row['project']}';";
+		$result3 = mysql_query($query3);
+		if ($row3 = mysql_fetch_assoc($result3)){
+			$row['project_name'] = $row3['name'];
+		} else {
+			$row['project_name'] = 'unknown';
+		}
+		
+		$buglist[$index++] = $row;				// add this row to buglist
+	}
+	return $buglist;
+}
--- templates_c/%%7C^7CF^7CFE2F9D%%buglist.html.php
+++ templates_c/%%7C^7CF^7CFE2F9D%%buglist.html.php
@@ -1,5 +1,7 @@
-<?php /* Smarty version 2.6.26, created on 2010-04-19 00:44:22
+<?php /* Smarty version 2.6.26, created on 2010-04-22 02:20:46
          compiled from buglist.html */ ?>
+<?php require_once(SMARTY_CORE_DIR . 'core.load_plugins.php');
+smarty_core_load_plugins(array('plugins' => array(array('modifier', 'replace', 'buglist.html', 16, false),array('modifier', 'date_format', 'buglist.html', 23, false),)), $this); ?>
 <div id='contentSpacer'>
 	<h2>Bugs assigned to you -</h2>
 	<div id='buglist'>
@@ -13,6 +15,48 @@
 				<div class='l_category'> Category </div>
 				<div class='l_lastupdate'> Last updated </div>
 			</li>
+			
+			<?php unset($this->_sections['i']);
+$this->_sections['i']['name'] = 'i';
+$this->_sections['i']['loop'] = is_array($_loop=$this->_tpl_vars['buglist']) ? count($_loop) : max(0, (int)$_loop); unset($_loop);
+$this->_sections['i']['show'] = true;
+$this->_sections['i']['max'] = $this->_sections['i']['loop'];
+$this->_sections['i']['step'] = 1;
+$this->_sections['i']['start'] = $this->_sections['i']['step'] > 0 ? 0 : $this->_sections['i']['loop']-1;
+if ($this->_sections['i']['show']) {
+    $this->_sections['i']['total'] = $this->_sections['i']['loop'];
+    if ($this->_sections['i']['total'] == 0)
+        $this->_sections['i']['show'] = false;
+} else
+    $this->_sections['i']['total'] = 0;
+if ($this->_sections['i']['show']):
+
+            for ($this->_sections['i']['index'] = $this->_sections['i']['start'], $this->_sections['i']['iteration'] = 1;
+                 $this->_sections['i']['iteration'] <= $this->_sections['i']['total'];
+                 $this->_sections['i']['index'] += $this->_sections['i']['step'], $this->_sections['i']['iteration']++):
+$this->_sections['i']['rownum'] = $this->_sections['i']['iteration'];
+$this->_sections['i']['index_prev'] = $this->_sections['i']['index'] - $this->_sections['i']['step'];
+$this->_sections['i']['index_next'] = $this->_sections['i']['index'] + $this->_sections['i']['step'];
+$this->_sections['i']['first']      = ($this->_sections['i']['iteration'] == 1);
+$this->_sections['i']['last']       = ($this->_sections['i']['iteration'] == $this->_sections['i']['total']);
+?>
+				<li class='<?php echo ((is_array($_tmp=$this->_tpl_vars['buglist'][$this->_sections['i']['index']]['status'])) ? $this->_run_mod_handler('replace', true, $_tmp, ' ', '') : smarty_modifier_replace($_tmp, ' ', '')); ?>
+'>
+				<div class='l_bid'> #<?php echo $this->_tpl_vars['buglist'][$this->_sections['i']['index']]['id']; ?>
+ </div>
+				<div class='l_status'> <?php echo $this->_tpl_vars['buglist'][$this->_sections['i']['index']]['status']; ?>
+ </div>
+				<div class='l_affects'> <?php echo $this->_tpl_vars['buglist'][$this->_sections['i']['index']]['project_name']; ?>
+ </div>
+				<div class='l_priority'> <?php echo $this->_tpl_vars['buglist'][$this->_sections['i']['index']]['priority']; ?>
+ </div>
+				<div class='l_subject'> <?php echo $this->_tpl_vars['buglist'][$this->_sections['i']['index']]['bugName']; ?>
+ </div>
+				<div class='l_category'> <?php echo $this->_tpl_vars['buglist'][$this->_sections['i']['index']]['bugType']; ?>
+ </div>
+				<div class='l_lastupdate'> <?php echo ((is_array($_tmp=$this->_tpl_vars['buglist'][$this->_sections['i']['index']]['timestamp'])) ? $this->_run_mod_handler('date_format', true, $_tmp, "%Y/%m/%d") : smarty_modifier_date_format($_tmp, "%Y/%m/%d")); ?>
+ </div>
+			<?php endfor; endif; ?>
 			
 			<li class='open'>
 				<div class='l_bid'> #34 </div>
