b96fde36acb58a9746a6f2c55be7175ebb01886d
Abhishek Mishra <ideamonk@gmail.com>
2010-04-24 14:23:12 +0530

    fixed auth control on all pages, turned it into a one-liner instead of whole big jumbo scope, making code more readable and unambiguous

--- index.php
+++ index.php
@@ -23,18 +23,21 @@ if (!isset($_GET['page']))
 else
 	$page = $_GET['page'];
 
+// Default page if no match - 
+$default_location = "/index.php?page=home";
+
 switch ($page){
 	case 'confirm':
 		checkLogin();
 		if (loggedIn()){
-			header("Location: /index.php?page=home");
+			header("Location: $default_location");
 		} else {
 			$smarty->assign('login_error', 'Invalid username or password.');
 		}
 		break;
 	
 	case 'home':
-		if (loggedIn()){
+		if (loggedIn()){ 
 			$template = 'home.html';
 			$smarty->assign('subtitle', 'Home');
 			$smarty->assign('my_open_count', getMyBugCount('open'));
@@ -49,65 +52,63 @@ switch ($page){
 		break;
 	
 	case 'reportNew':
-		if (loggedIn()){
-			$template = 'addedit.html';
-			$smarty->assign('subtitle','Add new bug');
-			$smarty->assign('scripts', array('/static/scripts/addnew.js'));
-			$_SESSION['currentpage']='reportNew';
-			
-			// put in form data for select options
-			$formdata = getNewFormData();
-			foreach($formdata as $key=>$val){
-				$smarty->assign($key, $val);
-			}
+		if (!loggedIn()){ header("Location: $default_location"); }
+		$template = 'addedit.html';
+		$smarty->assign('subtitle','Add new bug');
+		$smarty->assign('scripts', array('/static/scripts/addnew.js'));
+		$_SESSION['currentpage']='reportNew';
+		
+		// put in form data for select options
+		$formdata = getNewFormData();
+		foreach($formdata as $key=>$val){
+			$smarty->assign($key, $val);
 		}
 		break;
 	
 	case 'addNew':
-		if (loggedIn()){
-			if (($bug_id = addNewBug()) > 0){
-				header("Location: /index.php?page=list&highlight=$bug_id");
-			}
-			// otherwise case never comes as it dies in addNewBug itself
+		if (!loggedIn()){ header("Location: $default_location"); }
+		if (($bug_id = addNewBug()) > 0){
+			header("Location: /index.php?page=list&highlight=$bug_id");
 		}
+		// otherwise case never comes as it dies in addNewBug itself
 		break;
 		
 	case 'list':
-		if (loggedIn()){
-			$template = 'buglist.html';
-			if (!isset($_GET['filter'])){
-				// when no filter is set on buglist, show all of them
-				$smarty->assign('subtitle','All bugs');
-				$smarty->assign('buglist_heading', 'All bugs');
-				$smarty->assign('buglist',getAllBugList());
-				$_SESSION['currentpage']='allbugs';
-			} else {
-				switch ($_GET['filter']){
-					case 'self':
-						$smarty->assign('subtitle','Bugs related to you');
-						$smarty->assign('buglist_heading', 'Bugs related to you');
-						$smarty->assign('buglist',getMyBugList());
-						break;
-					case 'self_open':
-						$smarty->assign('subtitle','Your open bugs');
-						$smarty->assign('buglist_heading', 'Currently open bugs assigned to you');
-						$smarty->assign('buglist',getMyOpenBugList());
-						break;
-					case 'self_fixed':
-						$smarty->assign('subtitle','Bugs fixed by you');
-						$smarty->assign('buglist_heading', 'Bugs fixed by you');
-						$smarty->assign('buglist',getMyFixedBugList());
-						break;
-				}
+		if (!loggedIn()){ header("Location: $default_location"); }
+		$template = 'buglist.html';
+		if (!isset($_GET['filter'])){
+			// when no filter is set on buglist, show all of them
+			$smarty->assign('subtitle','All bugs');
+			$smarty->assign('buglist_heading', 'All bugs');
+			$smarty->assign('buglist',getAllBugList());
+			$_SESSION['currentpage']='allbugs';
+		} else {
+			switch ($_GET['filter']){
+				case 'self':
+					$smarty->assign('subtitle','Bugs related to you');
+					$smarty->assign('buglist_heading', 'Bugs related to you');
+					$smarty->assign('buglist',getMyBugList());
+					break;
+				case 'self_open':
+					$smarty->assign('subtitle','Your open bugs');
+					$smarty->assign('buglist_heading', 'Currently open bugs assigned to you');
+					$smarty->assign('buglist',getMyOpenBugList());
+					break;
+				case 'self_fixed':
+					$smarty->assign('subtitle','Bugs fixed by you');
+					$smarty->assign('buglist_heading', 'Bugs fixed by you');
+					$smarty->assign('buglist',getMyFixedBugList());
+					break;
 			}
-			// hilighting is common to all
-			if (isset($_GET['highlight'])){
-					$smarty->assign('highlight',$_GET['highlight']);
-			}
+		}
+		// hilighting is common to all
+		if (isset($_GET['highlight'])){
+				$smarty->assign('highlight',$_GET['highlight']);
 		}
 		break;
 		
 		case 'projects':
+			if (!loggedIn()){ header("Location: $default_location"); }
 			$template = 'projectlist.html';
 			$smarty->assign('subtitle','Projects');
 			$smarty->assign('projectlist_heading', 'All projects');
@@ -116,6 +117,7 @@ switch ($page){
 			break;
 			
 		case 'bug':
+			if (!loggedIn()){ header("Location: $default_location"); }
 			$template = 'bug.html';
 			$_SESSION['currentpage']='';
 			if (isset($_GET['bug_id'])){
